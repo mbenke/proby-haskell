8b5eb6294573ab7cbf41e03f874c9a04795f7b5a
Marcin Benke <ben@mimuw.edu.pl>
2013-04-16 17:29:42 +0200

    TH/QQ/Expr2: rearrange

--- TH/QQ/Expr2.hs
+++ TH/QQ/Expr2.hs
@@ -6,6 +6,7 @@ import Data.Typeable
 import Data.Data
 import Control.Applicative((<$>),(*>),(<*))
 
+-- show
 data Exp = EInt Int 
          | EAdd Exp Exp
          | ESub Exp Exp
@@ -13,26 +14,31 @@ data Exp = EInt Int
          | EDiv Exp Exp
          | EMetaVar String
            deriving(Show,Typeable,Data)
+                   
+pExp :: Parser Exp
+-- /show
+pExp = pTerm `chainl1` spaced addop
+
+pTerm = spaced pFactor `chainl1` spaced mulop
+
+pFactor = pNum <|> pMetaVar
+
+addop :: Parser (Exp->Exp->Exp)
+addop = fmap (const EAdd) (char '+')
+          <|> fmap (const ESub) (char '-')
+            
+mulop :: Parser (Exp->Exp->Exp)
+mulop = pOps [EMul,EDiv] ['*','/']
 
 pNum :: Parser Exp
 pNum = fmap (EInt . digitToInt) digit
 
-pFactor = pNum <|> pMetaVar
 pMetaVar = char '$' >> EMetaVar <$> ident
 
 small   = lower <|> char '_'
 large   = upper
 idchar  = small <|> large <|> digit <|> char '\''
 ident = do { c <- small; cs <- many idchar; return (c:cs) }
-
-pExp = pTerm `chainl1` spaced addop
-addop :: Parser (Exp->Exp->Exp)
-addop   =   fmap (const EAdd) (char '+')
-          <|> fmap (const ESub) (char '-')
-            
-pTerm = spaced pFactor `chainl1` spaced mulop
-mulop :: Parser (Exp->Exp->Exp)
-mulop = pOps [EMul,EDiv] ['*','/']
 
 pOps :: [a] -> [Char] -> Parser a
 pOps fs cs = foldr1 (<|>) $ map pOp $ zip fs cs
