0784a7ab57b0fe0e5dfeac2d21fc3ff569dc552a
Marcin Benke <marcin.benke@gmail.com>
2013-09-26 13:29:05 +0200

    Haslide: writer monad

--- Haslide/Base.hs
+++ Haslide/Base.hs
@@ -3,13 +3,15 @@ module Base where
 import Data.Text (Text)
 import qualified Data.Text as T
 import Data.Monoid
+import Control.Monad.Writer
 import Data.String
 import Str
  
 data Tex = Raw Text | Beside Tex Tex | Above Tex Tex | Empty
+type TeX = Tex
 instance IsString Tex where
   fromString = Raw . fromString
-  
+
 renderTex :: Tex -> Text
 renderTex Empty = T.empty
 renderTex (Raw t) = t
@@ -21,3 +23,14 @@ renderTexStr = T.unpack . renderTex
 instance Monoid Tex where
   mempty = Empty
   mappend = Beside
+
+type TexW = Writer Tex
+execTexW :: TexW () -> Tex
+execTexW = execWriter
+
+line :: TeX -> TexW ()
+line t = tell $ t <> "\n"
+{- might try to make strings writers, but this seems not  to work...
+instance IsString (Writer Tex ()) where
+         fromString = tell . fromString
+-}
